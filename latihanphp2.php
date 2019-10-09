26e7f211160745a9571e4927a92c043ab52bec46
Fikriyasa <53595190+fikriyasa@users.noreply.github.com>
2019-10-09 08:54:09 +0700

    Create latihanphp2.php

---
+++ latihanphp2.php
@@ -0,0 +1,30 @@
+<?php
+    //LOOPING /pengulangan
+    
+    //type 1 = for
+
+   // for($i=1; $i <=10; $i++) {
+     //   echo $i. "nama kalian <br><br>";
+    //}
+
+    //type 2 = while 
+    //$i = 1;
+    //while($i <=10 ){
+        //echo $i . "nama kalian <br><br>";
+        //$i++;
+    //}
+
+   // type 3 = foreach
+
+    //condicional/pengkonndisian
+    $angka = 9;
+    if($angka > 10 ) {
+        echo "lebih besar dari 10";
+    } else if ($angka == 10) {
+            echo "sama dengan 10";
+    } else {
+        echo "kurang dari 10";
+    }
+    
+    
+?>
